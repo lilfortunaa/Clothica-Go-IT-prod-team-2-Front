4f506a298958d687859f9ae9053c97007335ee56
lilfortunaa <bogdanchikzaichik@gmail.com>
2025-11-10 13:18:41 +0100

    PopularGoods

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,61 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Head from "next/head";
+import PopularGoods from "@/components/PopularGoods/PopularGoods";
 
 export default function Home() {
+  const mockGoods = [
+    {
+      id: 1,
+      title: "Базова футболка Clothica",
+      price: 1499,
+      rating: 5,
+      reviews: 2,
+      image: "/images/goods/good1-desk.jpg", 
+    },
+    {
+    id: 2,
+    title: "Класичне худі Clothica",
+    price: 1499,
+    rating: 5,
+    reviews: 2,
+    image: "/images/goods/good2-desk.jpg",
+  },
+  {
+    id: 3,
+    title: "Джинси slim fit Clothica",
+    price: 1499,
+    rating: 5,
+    reviews: 2,
+    image: "/images/goods/good3-desk.jpg",
+  },
+  {
+    id: 4,
+    title: "Світшот Clothica",
+    price: 1499,
+    rating: 5,
+    reviews: 2,
+    image: "/images/goods/good4-desk.jpg",
+  },
+  {
+      id: 5,
+      title: "Літні шорти Clothica",
+      price: 1499,
+      rating: 5,
+      reviews: 2,
+      image: "/images/goods/good5-desk.jpg", 
+    },
+    {
+      id: 6,
+      title: "Легка куртка Clothica",
+      price: 1499,
+      rating: 5,
+      reviews: 2,
+      image: "/images/goods/good6-desk.jpg", 
+    },
+  ];
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div>
+      <PopularGoods goods={mockGoods} />
     </div>
   );
 }

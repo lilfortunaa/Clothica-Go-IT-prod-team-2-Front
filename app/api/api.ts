6b5b4c7177d3fb0afc42df965aab4ae1ff22fe19
Artem <tem4ik20.1@gmail.com>
2025-11-13 10:49:16 +0200

    fix

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.BACKEND_API_URL,
+  baseURL: process.env.BACKEND_API_URL + '/api',
   withCredentials: true,
 });
 
